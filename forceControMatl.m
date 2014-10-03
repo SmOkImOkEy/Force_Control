6a5e929f449086de7ad9b07c984b6360efcaecc7
SmOkImOkEy <ronmisgav@gmail.com>
2014-10-03 13:36:05 +0300

    simulink testing 1.1

--- forceControMatl.m
+++ forceControMatl.m
@@ -47,8 +47,9 @@ disp('setting constants...')
 disp('predefined path')
     Route.start_time=0;
     Route.end_time=en;      
-    Route.xfun=@(t) 1/(2*m)*t.^2;   
-    Route.yfun=@(t) 2/(2*m)*t.^2; 
+    Route.xfun=@(t) 2*sin(4*pi/en*t);   
+    Route.yfun=@(t) 1+cos(2*pi/en*t); 
+    
     tt=(Route.start_time:dt:Route.end_time);  
     x=Route.xfun(tt)-Route.xfun(Route.start_time); % route x samples
     y=Route.yfun(tt)-Route.yfun(Route.start_time); % route y samples 
